74d0d636b5d940445fafcf52f44a629a5e75b79a
Stephen Quan <squan@esri.com>
2019-04-08 11:13:43 +1000

    Use consistent indentation

--- SimpleScriptSite.cpp
+++ SimpleScriptSite.cpp
@@ -3,32 +3,32 @@
 
 STDMETHODIMP_(ULONG) CSimpleScriptSite::AddRef()
 {
-	return InterlockedIncrement(&m_cRefCount);
+    return InterlockedIncrement(&m_cRefCount);
 }
 
 STDMETHODIMP_(ULONG) CSimpleScriptSite::Release()
 {
-	if (!InterlockedDecrement(&m_cRefCount))
-	{
-		delete this;
-		return 0;
-	}
-	return m_cRefCount;
+    if (!InterlockedDecrement(&m_cRefCount))
+    {
+        delete this;
+        return 0;
+    }
+    return m_cRefCount;
 }
 
 STDMETHODIMP CSimpleScriptSite::QueryInterface(REFIID riid, void **ppvObject)
 {
-	if (riid == IID_IUnknown || riid == IID_IActiveScriptSiteWindow)
-	{
-		*ppvObject = (IActiveScriptSiteWindow *)this;
-		AddRef();
-		return NOERROR;
-	}
-	if (riid == IID_IActiveScriptSite)
-	{
-		*ppvObject = (IActiveScriptSite *)this;
-		AddRef();
-		return NOERROR;
-	}
-	return E_NOINTERFACE;
+    if (riid == IID_IUnknown || riid == IID_IActiveScriptSiteWindow)
+    {
+        *ppvObject = (IActiveScriptSiteWindow *)this;
+        AddRef();
+        return NOERROR;
+    }
+    if (riid == IID_IActiveScriptSite)
+    {
+        *ppvObject = (IActiveScriptSite *)this;
+        AddRef();
+        return NOERROR;
+    }
+    return E_NOINTERFACE;
 }
--- SimpleScriptSite.h
+++ SimpleScriptSite.h
@@ -2,41 +2,41 @@
 #define CSimpleScriptSite_H
 
 class CSimpleScriptSite :
-	public IActiveScriptSite,
-	public IActiveScriptSiteWindow
+    public IActiveScriptSite,
+    public IActiveScriptSiteWindow
 {
 public:
-	CSimpleScriptSite() : m_cRefCount(1), m_hWnd(NULL) { }
+    CSimpleScriptSite() : m_cRefCount(1), m_hWnd(NULL) { }
 
-	// IUnknown
+    // IUnknown
 
-	STDMETHOD_(ULONG, AddRef)();
-	STDMETHOD_(ULONG, Release)();
-	STDMETHOD(QueryInterface)(REFIID riid, void **ppvObject);
+    STDMETHOD_(ULONG, AddRef)();
+    STDMETHOD_(ULONG, Release)();
+    STDMETHOD(QueryInterface)(REFIID riid, void **ppvObject);
 
-	// IActiveScriptSite
+    // IActiveScriptSite
 
-	STDMETHOD(GetLCID)(LCID *plcid) { *plcid = 0; return S_OK; }
-	STDMETHOD(GetItemInfo)(LPCOLESTR pstrName, DWORD dwReturnMask, IUnknown **ppiunkItem, ITypeInfo **ppti) { return TYPE_E_ELEMENTNOTFOUND; }
-	STDMETHOD(GetDocVersionString)(BSTR *pbstrVersion) { *pbstrVersion = SysAllocString(L"1.0"); return S_OK; }
-	STDMETHOD(OnScriptTerminate)(const VARIANT *pvarResult, const EXCEPINFO *pexcepinfo) { return S_OK; }
-	STDMETHOD(OnStateChange)(SCRIPTSTATE ssScriptState) { return S_OK; }
-	STDMETHOD(OnScriptError)(IActiveScriptError *pIActiveScriptError) { return S_OK; }
-	STDMETHOD(OnEnterScript)(void) { return S_OK; }
-	STDMETHOD(OnLeaveScript)(void) { return S_OK; }
+    STDMETHOD(GetLCID)(LCID *plcid) { *plcid = 0; return S_OK; }
+    STDMETHOD(GetItemInfo)(LPCOLESTR pstrName, DWORD dwReturnMask, IUnknown **ppiunkItem, ITypeInfo **ppti) { return TYPE_E_ELEMENTNOTFOUND; }
+    STDMETHOD(GetDocVersionString)(BSTR *pbstrVersion) { *pbstrVersion = SysAllocString(L"1.0"); return S_OK; }
+    STDMETHOD(OnScriptTerminate)(const VARIANT *pvarResult, const EXCEPINFO *pexcepinfo) { return S_OK; }
+    STDMETHOD(OnStateChange)(SCRIPTSTATE ssScriptState) { return S_OK; }
+    STDMETHOD(OnScriptError)(IActiveScriptError *pIActiveScriptError) { return S_OK; }
+    STDMETHOD(OnEnterScript)(void) { return S_OK; }
+    STDMETHOD(OnLeaveScript)(void) { return S_OK; }
 
-	// IActiveScriptSiteWindow
+    // IActiveScriptSiteWindow
 
-	STDMETHOD(GetWindow)(HWND *phWnd) { *phWnd = m_hWnd; return S_OK; }
-	STDMETHOD(EnableModeless)(BOOL fEnable) { return S_OK; }
+    STDMETHOD(GetWindow)(HWND *phWnd) { *phWnd = m_hWnd; return S_OK; }
+    STDMETHOD(EnableModeless)(BOOL fEnable) { return S_OK; }
 
-	// Miscellaneous
+    // Miscellaneous
 
-	HRESULT SetWindow(HWND hWnd) { m_hWnd = hWnd; return S_OK; }
+    HRESULT SetWindow(HWND hWnd) { m_hWnd = hWnd; return S_OK; }
 
 public:
-	LONG m_cRefCount;
-	HWND m_hWnd;
+    LONG m_cRefCount;
+    HWND m_hWnd;
 };
 
 #endif
